e158203f37b2213e316380669e757255721ed096
Fratean Razvan <47633722+IronTwist@users.noreply.github.com>
2021-07-09 09:48:48 +0300

    Add files via upload

---
+++ HugeNumbersIterator.php
@@ -0,0 +1,34 @@
+<?php
+
+function randomNumbersGenerator($numbersReturn): iterable
+{
+    $x = 0;
+    $holdNumber = 0;
+
+    while($x <= $numbersReturn){
+        if($x > $holdNumber && ($x - $holdNumber) <= 10){
+            yield $x;
+            $holdNumber = $x;
+        }
+
+        if($x >= $numbersReturn - 1){
+            exit();
+        }
+
+        $x = mt_rand(1, $numbersReturn);
+    }
+}
+
+function consumerIterable(iterable $iterable): array
+{
+
+    foreach ($iterable as $valueIterable) {
+        echo $valueIterable.' ';
+    }
+
+}
+
+$numbersToReturn = 100;
+
+$get = consumerIterable(randomNumbersGenerator($numbersToReturn));
+print_r($get);
